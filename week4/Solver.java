835e63e29d8ea83c84b364a136da823a42a66c53
LeeReindeer <reindeerlee.work@gmail.com>
2018-05-13 16:19:38 +0800

    [fix] Use stack interator in FILO order

--- week4/Solver.java
+++ week4/Solver.java
@@ -1,9 +1,9 @@
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.MinPQ;
+import edu.princeton.cs.algs4.Stack;
 import edu.princeton.cs.algs4.StdOut;
 
 import java.util.Comparator;
-import java.util.Stack;
 
 /**
  * @author leer
